7d600dc36e39ac3207b23781f5eb2a0851812c94
ni3tRinoo <vladik07@yahoo.com>
2023-07-24 17:41:40 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//1
+
+// let result = 5 + 5 + 5;
+
+// console.log(result);
+
+//2
+// const email = "vladik07@yahoo.com";
+
+// console.log(email);
+// console.log(email.length);
+
+//3
+
+// let My = "my";
+
+// let name = "name";
+
+// let is = "is";
+
+// let fullName = " my name is Viktor";
+
+// console.log(fullName);
+
+//4
+
+// let userName = "ni3tRinoo, you have to pay 100$!"
+
+// alert(userName);
